50cdd5df612fe98c1cf701213ae8b372d6d8b7ad
Di_Masta <vvladoh@gmail.com>
2012-11-08 00:02:13 +0200

    Start point of project DOR!

---
+++ src/mt/dor/game/Dead_or_GloryActivity.java
@@ -0,0 +1,13 @@
+package mt.dor.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Dead_or_GloryActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
