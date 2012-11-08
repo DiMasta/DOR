f72b1209d0f99a5c90cc9cca0ba5797e16053179
Di_Masta <vvladoh@gmail.com>
2012-11-09 00:58:49 +0200

    Character list started added Name and race chooser!

--- gen/mt/dor/game/R.java
+++ gen/mt/dor/game/R.java
@@ -8,16 +8,29 @@
 package mt.dor.game;
 
 public final class R {
+    public static final class array {
+        public static final int races_array=0x7f060000;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int background=0x7f040000;
+        public static final int black=0x7f040001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+    }
+    public static final class id {
+        public static final int chartacter_label=0x7f070000;
+        public static final int race_spinner=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int welcome_message=0x7f040000;
+        public static final int app_name=0x7f050000;
+        public static final int character_name_label=0x7f050001;
+        public static final int name_hint=0x7f050002;
+        public static final int race_label=0x7f050003;
     }
 }
